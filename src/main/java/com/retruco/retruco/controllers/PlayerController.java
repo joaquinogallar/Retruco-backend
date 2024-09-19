c33387158cc3930a81d4ae504fa27ed9ec7b23a8
Joaquin <joaquinnogallar@gmail.com>
2024-09-19 16:38:10 -0300

    feat: CrossOrigin in PlayerController so the front can access data endpoints

--- src/main/java/com/retruco/retruco/controllers/PlayerController.java
+++ src/main/java/com/retruco/retruco/controllers/PlayerController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/players")
+@CrossOrigin(origins = "http://localhost:5173") // necesario para permitirle al front consumier nuestros endpoints
 public class PlayerController {
 
     @Autowired
